56a242a103c808a6d79514123203de673e1893e9
Nelson <n.sanchezesparza@gmail.com>
2023-10-10 22:10:00 -0500

    06-angular-attribute-binding

---
+++ 06-binding/06-angular-attribute-binding/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  h = 300;
+  w = 200;
+  bdr = 5;
+  clspn = 2;
+}
