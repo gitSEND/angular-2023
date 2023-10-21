4763691f544bc4965ebe9ca237ecec2abcd4099b
Nelson <n.sanchezesparza@gmail.com>
2023-10-21 14:58:45 -0500

    11-angular-ngplural

---
+++ 07-diretives/11-angular-ngplural/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  itemNum = 0;
+  personNum = 1;
+  minutes = 5;
+}
